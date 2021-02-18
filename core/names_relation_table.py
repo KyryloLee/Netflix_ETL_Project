bfba3de0c1002457c290b7b1f03dab2394f9e9b2
Kyrylo Lee <lee@MacBook-Air-Kyrylo.local>
2021-02-18 11:22:04 +0200

    Delete db

--- core/names_relation_table.py
+++ core/names_relation_table.py
@@ -1,12 +1,11 @@
 from .base_table import BaseTable
 
-class IdToNameRelation(BaseTable):
-    def __init__(self, table_name, cur, ):
+class NamesRelation(BaseTable):
+    def __init__(self, table_name, table1: str, table1_id:str, table2:str, table2_name:str):
         self.table_name = table_name
         self.col = [
             'id text not null',
             'name text',
-
         ]
 
         BaseTable.__init__(cur)
