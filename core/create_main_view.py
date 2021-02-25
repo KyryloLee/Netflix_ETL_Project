20b7a5ac12011bc20c924624981504abed1b837a
Kyrylo Lee <lee@MacBook-Air-Kyrylo.local>
2021-02-25 14:25:34 +0200

    optimazed create view

--- core/create_main_view.py
+++ core/create_main_view.py
@@ -2,41 +2,25 @@ def create_main_view(cur):
     cmd = """
 create view if not exists main_view
 as
-    select 
-        m.show_id,
-        m.type,
-        m.title,
-        m_d.director,
-        m_c.actors as 'cast',
-        m_cntr.country,
-        m.date_added,
-        m.release_year,
-        m.rating,
-        m.duration,
-        m.seasons,
-        m_g.listed_in
-    from Movies m
-    left join (
-        select id, group_concat(name, ', ') director 
-        from Movie_directors
-        group by id
-    ) m_d on m.show_id = m_d.id
-    left join (
-        select id, group_concat(name, ', ') actors 
-        from Movie_cast
-        group by id
-    ) m_c on m.show_id = m_c.id
-    left join (
-        select id, group_concat(name, ', ') country  
-        from Movie_country
-        group by id
-    ) m_cntr on m.show_id = m_cntr.id
-    left join (
-        select id, group_concat(name, ', ') listed_in
-        from Movie_genre
-        group by id
-    ) m_g on m.show_id = m_g.id
-    group by m.show_id;
-    
+    select
+        t1.show_id, t1.type, t1.title,
+        group_concat(distinct t2.name) director,
+        group_concat(distinct t3.name) as cast,
+        group_concat(distinct t4.name) country,
+        t1.date_added, t1.release_year, t1.rating, 
+        case
+            when t1.seasons = 1 then cast(t1.seasons as text) || ' Season'
+            when t1.seasons > 1 then cast(t1.seasons as text) || ' Seasons'
+            else cast(t1.duration as text) || ' min'
+        end duration,
+        group_concat(distinct t5.name) listed_in
+    from
+        Movies t1,
+        Movie_directors t2,
+        Movie_cast t3,
+        Movie_country t4,
+        Movie_genre t5
+    where (t1.show_id, t1.show_id, t1.show_id, t1.show_id) = (t2.id, t3.id, t4.id, t5.id)
+    group by t1.show_id;
 """
     cur.execute(cmd)
