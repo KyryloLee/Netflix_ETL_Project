7d49f50c003ce8de774e5db615753ad2f712087f
Kyrylo Lee <lee@MacBook-Air-Kyrylo.local>
2021-03-02 12:19:36 +0200

    added comments

--- main.py
+++ main.py
@@ -5,18 +5,19 @@ from csv import DictReader
 from core import create_connection, UniqueNameTable,\
     UniqueIdTable, Movies, IdToNamesRelation, create_main_view
 
+# path to csv file
 RAW_DATA = path.join(getcwd(), 'raw_data', 'netflix_titles.csv')
+# path to db
 DB_PATH = path.join(getcwd(), 'db', 'Netflix.db')
 
+# Column to table relations
 COL_TO_TABLE_NAMES = {
     'type': 'Types',
     'rating': 'Ratings'
 }
-
 INT_COL_TO_TABLE_NAMES = {
     'release_year': 'Release_years'
 }
-
 COL_WITH_MULTY_VALUES_TO_TABLE_NAMES = {
     'director': 'Directors',
     'cast': 'Actors',
@@ -24,6 +25,7 @@ COL_WITH_MULTY_VALUES_TO_TABLE_NAMES = {
     'listed_in': 'Genres'
 }
 
+# Relation between tables
 RELATION_COLUMNS_TO_TABLE = [
     ('Movie_directors', ('Movies','Directors'), ('show_id','director')),
     ('Movie_cast', ('Movies','Actors'), ('show_id','cast')),
@@ -40,15 +42,17 @@ def insert_data_into_tables(row: dict, tables: dict):
     """
     for k,table in tables.items():
         
+        # if it unique name, add to related table
         if k in COL_TO_TABLE_NAMES.keys():
             table.insert_data([row[k].strip()])
-
+        # if it unqiue id or number, add to related table
         elif k in INT_COL_TO_TABLE_NAMES.keys():
             table.insert_data([int(row[k])])
-
+        # parse stirng and add data to table
         elif k in COL_WITH_MULTY_VALUES_TO_TABLE_NAMES.keys():
             for value in row[k].split(','):
                 table.insert_data([value.strip()])
+        # add data to main movie table
         else:
             table.parse_and_insert_data(row)
 
@@ -65,11 +69,15 @@ def insert_data_into_relation_tables(row: dict, relation: list):
             table.insert_data([movie_id, x.strip()])
 
 if __name__=='__main__':
+    """
+    The program opens the csv file, parse it and fill related architecture tables.
+    """
     conn = create_connection(DB_PATH)
 
     with conn:
         cursor = conn.cursor()
 
+        # Connect to tables
         tables = {k: UniqueNameTable(v, cursor) for k,v in COL_TO_TABLE_NAMES.items()}
         tables.update({k: UniqueIdTable(v, cursor) for k,v in INT_COL_TO_TABLE_NAMES.items()})
         tables.update({k: UniqueNameTable(v, cursor) for k,v in COL_WITH_MULTY_VALUES_TO_TABLE_NAMES.items()})
